f7ea3ea0f278e2384f73bcf97a720a770295b8d1
vinicius-toledo-paes <143434075+vinicius-toledo-paes@users.noreply.github.com>
2025-01-16 14:45:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,16 +4,20 @@
 #include "genetic.h"
 #include "greedy.h"
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
     char heuristica[8];
-    
-    if (argc < 2){
+
+    if (argc < 2)
+    {
         printf("ERRO: nome de arquivo não encontrado \n\n");
         exit(1);
-    } else if (argc < 3){
-        if (rand()%2){
+    }
+    else if (argc < 3)
+    {
+        if (rand() % 2)
+        {
             heuristica[0] = 'g';
             heuristica[1] = 'r';
             heuristica[2] = 'e';
@@ -21,7 +25,9 @@ int main(int argc, char *argv[]){
             heuristica[4] = 'd';
             heuristica[5] = 'y';
             heuristica[6] = '\0';
-        } else {
+        }
+        else
+        {
             heuristica[0] = 'g';
             heuristica[1] = 'e';
             heuristica[2] = 'n';
@@ -31,19 +37,24 @@ int main(int argc, char *argv[]){
             heuristica[6] = 'c';
             heuristica[7] = '\0';
         }
-    } else {
-        for(char i = 0, c = argv[2][0]; c != '\0' && i < 7; i++, c = argv[2][i]){
+    }
+    else
+    {
+        for (char i = 0, c = argv[2][0]; c != '\0' && i < 7; i++, c = argv[2][i])
+        {
             heuristica[i] = c;
-            heuristica[i+1] = '\0';
+            heuristica[i + 1] = '\0';
         }
-        if (!(heuristica[0] == 'g' && (((heuristica[1] == 'r') && (heuristica[2] == 'e') && (heuristica[3] == 'e') && (heuristica[4] == 'd') && (heuristica[5] == 'y')) || ((heuristica[1] == 'e') && (heuristica[2] == 'n') && (heuristica[3] == 'e') && (heuristica[4] == 't') && (heuristica[5] == 'i') && (heuristica[6] == 'c'))))){
+        if (!(heuristica[0] == 'g' && (((heuristica[1] == 'r') && (heuristica[2] == 'e') && (heuristica[3] == 'e') && (heuristica[4] == 'd') && (heuristica[5] == 'y')) || ((heuristica[1] == 'e') && (heuristica[2] == 'n') && (heuristica[3] == 'e') && (heuristica[4] == 't') && (heuristica[5] == 'i') && (heuristica[6] == 'c')))))
+        {
             printf("ERRO: heuristica desconhecida");
             exit(1);
         }
     }
 
     FILE *fp = fopen(argv[1], "r");
-    if (!fp){
+    if (!fp)
+    {
         printf("ERRO: Não foi possível encontrar arquivo indicado");
         fclose(fp);
         exit(1);
@@ -56,40 +67,60 @@ int main(int argc, char *argv[]){
     int k = 0;
     char c = '\0';
     char num[10];
-    while(c != ' '){
+    while (c != ' ' && c != '\n')
+    {
         c = fgetc(fp);
-        if(c != EOF){
-            num[k] = c;
+        if (c == ' ' && !k)
+        {
+            while (c == ' ')
+            {
+                c = fgetc(fp);
+            }
         }
-        k++;
+        if (c != EOF && c != ' ' && c != '\n')
+        {
+            num[k] = c;
+            k++;
+        }
     }
     num[k] = '\0';
     sscanf(num, "%d", &literais);
     int clausulas;
     c = '\0';
     k = 0;
-    while(c != ' '){
+    while (c != ' ' && c != '\n')
+    {
         c = fgetc(fp);
-        if(c != EOF){
-            num[k] = c;
+        if (c == ' ' && !k)
+        {
+            while (c == ' ')
+            {
+                c = fgetc(fp);
+            }
         }
-        k++;
+        if (c != EOF && c != ' ' && c != '\n')
+        {
+            num[k] = c;
+            k++;
+        }
     }
     num[k] = '\0';
     sscanf(num, "%d", &clausulas);
 
     int j;
 
-    Literal *liter = (Literal *) malloc(literais * sizeof(Literal));
-    for(j = 0; j < literais; j ++){
+    Literal *liter = (Literal *)malloc(literais * sizeof(Literal));
+    for (j = 0; j < literais; j++)
+    {
         liter[j].id = j + 1;
         liter[j].valor = UNSOLVED;
     }
 
     Literals *lite;
     Literals **literals = &lite;
-    for(j = 0; j < literais; j++){
-        lite = (Literals *) malloc(sizeof(Literals));
+    for (j = 0; j < literais; j++)
+    {
+        lite = (Literals *)malloc(sizeof(Literals));
         lite->literal = &liter[j];
         lite->next = NULL;
         lite = lite->next;
@@ -102,50 +133,68 @@ int main(int argc, char *argv[]){
     Formula **formula = &nescau;
 
     int falta = clausulas;
-    while(falta){
+    while (falta)
+    {
         Clause *santa = NULL;
         Clause **clausula = &santa;
-        while(valor){
+        while (valor)
+        {
             valor = 0;
             k = 0;
-            while(c != ' ' && c != '\n'){
+            while (c != ' ' && c != '\n')
+            {
                 c = fgetc(fp);
-                if(c != EOF){
-                    num[k] = c;
+                if (c == ' ' && !k)
+                {
+                    while (c == ' ')
+                    {
+                        c = fgetc(fp);
+                    }
                 }
-                k++;
+                if (c != EOF && c != ' ' && c != '\n')
+                {
+                    num[k] = c;
+                    k++;
+                }
             }
             num[k] = '\0';
             sscanf(num, "%d", &valor);
-            if (valor > 0){
-                santa = (Clause *) malloc(sizeof(Clause));
+            if (valor > 0)
+            {
+                santa = (Clause *)malloc(sizeof(Clause));
                 santa->sinal = IDENTITY;
                 santa->literal = &liter[valor - 1];
                 santa->next = NULL;
                 santa = santa->next;
-            } else if (valor < 0){
-                santa = (Clause *) malloc(sizeof(Clause));
+            }
+            else if (valor < 0)
+            {
+                santa = (Clause *)malloc(sizeof(Clause));
                 santa->sinal = COMPLEMENT;
                 santa->literal = &liter[-(valor + 1)];
                 santa->next = NULL;
                 santa = santa->next;
             }
         }
-        if (*clausula){
+        if (*clausula)
+        {
             valor = 1;
-            nescau = (Formula *) malloc(sizeof(Formula));
+            nescau = (Formula *)malloc(sizeof(Formula));
             nescau->clausula = *clausula;
             nescau->next = NULL;
             nescau = nescau->next;
             falta--;
-        } else {
+        }
+        else
+        {
             falta = 0;
         }
     }
 
     int maxClauses;
 
-    if ((heuristica[0] == 'g') && (heuristica[1] == 'r') && (heuristica[2] == 'e') && (heuristica[3] == 'e') && (heuristica[4] == 'd') && (heuristica[5] == 'y')) {
+    if ((heuristica[0] == 'g') && (heuristica[1] == 'r') && (heuristica[2] == 'e') && (heuristica[3] == 'e') && (heuristica[4] == 'd') && (heuristica[5] == 'y'))
+    {
         printf("Heuristica utilizada: greedy");
         int repetitons = literais;
 
@@ -156,14 +205,15 @@ int main(int argc, char *argv[]){
         tempoFinal = clock();
     }
 
-    if ((heuristica[0] == 'g') && (heuristica[1] == 'e') && (heuristica[2] == 'n') && (heuristica[3] == 'e') && (heuristica[4] == 't') && (heuristica[5] == 'i') && (heuristica[6] == 'c')) {
+    if ((heuristica[0] == 'g') && (heuristica[1] == 'e') && (heuristica[2] == 'n') && (heuristica[3] == 'e') && (heuristica[4] == 't') && (heuristica[5] == 'i') && (heuristica[6] == 'c'))
+    {
         printf("Heuristica utilizada: genetic");
         int populationSize = clausulas + literais;
         int maxIterations = literais;
         int mrsDeath = literais;
         int mrLife = clausulas;
         int mutationProbability = literais;
-        
+
         tempoInicial = clock();
 
         maxClauses = bestGenes(populationSize, maxIterations, mrsDeath, mrLife, mutationProbability, *literals, *formula);
